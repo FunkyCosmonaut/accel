0414db1315b65606ae4190f00e80275e86d384b8
Christopher Munoz <32150826+FunkyCosmonaut@users.noreply.github.com>
2019-07-07 08:55:44 -0400

    unfinished

---
+++ Chapter3/quartiles.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+
+void getinputs(vector<double> input){
+				while(cin >> x)
+				input.push_back(x);
+}
+void sortinputs(vector<double> input){
+				sort(input.begin(), input.end());
+
+}
+
+void quartile(vector<double> input){
+}
+
+int main(){
+
+typedef vector<double>::size_type vec_sz;
